0ae0cf017438f56737cda0ce25cb90622ee10419
selimdilsad <selimdilsadercan@gmail.com>
2023-09-22 18:13:33 +0300

    fixed3: form reset in chapter form after submit

--- app/(dashboard)/teacher/courses/[courseId]/_components/chapter-form.tsx
+++ app/(dashboard)/teacher/courses/[courseId]/_components/chapter-form.tsx
@@ -72,7 +72,7 @@ const ChapterForm = ({ initialData }: Props) => {
       toast.error("Failed to create chapter");
     },
     onSuccess: () => {
-      form.reset({ title: "" });
+      form.resetField("title");
       toast.success("Chapter created");
       toggleCreating();
       router.refresh();
