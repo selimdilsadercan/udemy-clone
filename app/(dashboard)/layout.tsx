0d3b8b28392bec356002a0c20c532225ae8874cc
selimdilsad <selimdilsadercan@gmail.com>
2023-11-13 23:43:32 +0300

    changed sth..

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,14 @@
 import Navbar from "./zomponents/navbar";
 import Sidebar from "./zomponents/sidebar";
 
-interface Props {
-  children: React.ReactNode;
-}
-
-function Layout({ children }: Props) {
+function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
         <Navbar />
       </div>
 
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+      <div className="fixed inset-y-0 z-50 flex-col hidden w-56 h-full md:flex">
         <Sidebar />
       </div>
 
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -18,7 +18,7 @@ async function Page() {
 
   return (
     <div className="p-6 space-y-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         <InfoCard
           icon={Clock}
           label="In Progress"
